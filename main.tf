506eeec9961eec1075ea47dfba15186aeee98a2c
Mohammedirshaq <82198906+Mohammedirshaq@users.noreply.github.com>
2025-08-22 16:49:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+resource "google_container_cluster" "primary" {
+  name     = c3
+  location = us-west1
 
+  initial_node_count = 2
+
+  node_config {
+    machine_type = e2-standard-4
+    disk_size_gb = 20
+  }
+}
